b3dffb99d6755ca2ada6e52af5442454aa72e9d3
komodothon <kadir.v@gmail.com>
2024-11-15 15:40:41 +0530

    Many-To-Many

--- setup_db.py
+++ setup_db.py
@@ -1,88 +1,66 @@
 # imports
 
-import sqlite3
-
-from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, Table
 from sqlalchemy.orm import declarative_base, sessionmaker, relationship
 
-
-# sqlite raw codes
-
-connection = sqlite3.connect('example.db')
-cursor = connection.cursor()
-
-cursor.executescript("""
-    CREATE TABLE IF NOT EXISTS users_sql (
-                id INTEGER PRIMARY KEY, 
-                username TEXT NOT NULL, 
-                email TEXT NOT NULL
-    );
-
-    CREATE TABLE IF NOT EXISTS posts_sql (
-                post_id INTEGER PRIMARY KEY,
-                title TEXT NOT NULL,
-                content TEXT NOT NULL,
-                user_id INTEGER,
-                FOREIGN KEY (user_id) REFERENCES users_sql (id)
-    );
-""")
-
-cursor.execute("INSERT INTO users_sql (id, username, email) VALUES (?, ?, ?)", (4, 'Alice', 'alice@samplecom'))
-cursor.execute("INSERT INTO posts_sql (post_id, title, content, user_id) VALUES (?, ?, ?, ?)", (2, 'Today\'s rains', 'content content content', 4))
-
-connection.commit()
-connection.close()
-
-# SQLAlchemy 
+# SQLAlchemy Many-To-Many
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'users'
+# Association table
+student_course = Table(
+    'student_course', Base.metadata,
+    Column('student_id', Integer, ForeignKey('students.id')),
+    Column('course_id', Integer, ForeignKey('courses.id'))
+)
 
-    # define columns for table
+# student model
+class Student(Base):
+    __tablename__ = 'students'
+
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
+    name = Column(String, nullable=False)
     email = Column(String, unique=True, nullable=False)
 
-    posts = relationship("Post", back_populates="author")
+    # relationship
+    courses = relationship("Course", secondary=student_course, back_populates="students")
 
     def __repr__(self):
-        return f"<User(username='{self.username}', email='{self.email}')>"
+        return f"<Student(id={self.id}, name={self.name}, email={self.email})>"
+
+# course model
+class Course(Base):
+    __tablename__ = 'courses'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    details = Column(String, nullable=False)
+
+    # relationship
+    students = relationship("Student", secondary=student_course, back_populates="courses")
     
-class Post(Base):
-    __tablename__ = 'posts'
 
-    # define columns
-    post_id = Column(Integer, primary_key=True)
-    title = Column(String)
-    content = Column(String)
-    user_id = Column(Integer, ForeignKey('users.id'))
-
-    author = relationship("User", back_populates="posts")
-
-    
-
-engine = create_engine('sqlite:///example.db')
-
+engine = create_engine('sqlite:///courses.db')
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-new_user1 = User(username='Bob', email='Bob@example.com')
-print(new_user1)
-session.add(new_user1)
+stud1 = Student(name="alice", email="alice@sample.com")
+stud2 = Student(name="charl", email="charl@sample.com")
+stud3 = Student(name="dory", email="dory@sample.com")
+stud4 = Student(name="emma", email="emma@sample.com")
+
+course1 = Course(title="History 101", details="history")
+course2 = Course(title="Maths 101", details="maths")
+
+course2.students.extend([stud1, stud4])
+course1.students.extend([stud2, stud3, stud4])
+
+session.add_all([stud1, stud2, stud3, stud4])
 session.commit()
 
-user = session.query(User).filter_by(username='Bob').first()
-
-post1 = Post(title='First post', content='content in 1st post', author=user)
-session.add(post1)
-session.commit()
-
-post2 = Post(title='Second post', content=' 2nd post content')
-user.posts.append(post2)
+session.add_all([course1, course2])
 session.commit()
 
 session.close()
